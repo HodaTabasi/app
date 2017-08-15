a8702f729ba1668fa499507c1c6add70d6151418
ahmed <1>
2017-08-15 11:37:03 +0300

    Facebook Account Kit Configuration

--- app/src/main/java/com/shoppingapp/Activity/SplashActivtiy.java
+++ app/src/main/java/com/shoppingapp/Activity/SplashActivtiy.java
@@ -1,22 +1,26 @@
 package com.shoppingapp.Activity;
 
-
 import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 import com.shoppingapp.R;
 
-
-
 public class SplashActivtiy extends AppCompatActivity implements Animation.AnimationListener{
+    private  final int APP_REQUEST_CODE = 100;
     // Animation
         Animation anima;
         Context mContext;
@@ -35,30 +39,62 @@ private Thread mSplashThread;
         anima.setAnimationListener(SplashActivtiy.this);
         // start the animation
         logo.startAnimation(anima);
+        mSplashThread =  new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized(this){
+                        wait(2000);
+                    }
+                }
+                catch(InterruptedException ex){
+                }
 
 
+                if (AccountKit.getCurrentAccessToken() != null) {
+                    AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                        @Override
+                        public void onSuccess(final Account account) {
+                            Log.e("Error", "User Info Successfully");
 
-//    mSplashThread =  new Thread(){
-//            @Override
-//            public void run(){
-//                try {
-//                    synchronized(this){
-//                        wait(2000);
-//                    }
-//                }
-//                catch(InterruptedException ex){
-//                }
-//                finish();
-//
-//                Intent i = new Intent(SplashActivtiy.this,MainActivity.class);
-//                startActivity(i);
-//            }
-//        };
-//
-//        mSplashThread.start();
+                            finish();
+                            Intent i = new Intent(SplashActivtiy.this,MainActivity.class);
+                            startActivity(i);
+
+                        }
+
+                        @Override
+                        public void onError(final AccountKitError error) {
+                            Log.e("Error", error.getUserFacingMessage());
+                            Toast.makeText(getApplicationContext(), error.getUserFacingMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    verifyMobileNumber();
+                }
+
+ 
+
+            }
+        };
+
+        mSplashThread.start();
 
 
     }
+    private void verifyMobileNumber() {
+        final Intent intent = new Intent(this, AccountKitActivity.class);
+        final AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder
+                = new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                LoginType.PHONE,
+                AccountKitActivity.ResponseType.TOKEN);
+        final AccountKitConfiguration configuration = configurationBuilder.build();
+        intent.putExtra(
+                AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
+                configuration);
+        startActivityForResult(intent, APP_REQUEST_CODE);
+    }
+
 
     @Override
     public void onAnimationStart(Animation animation) {
@@ -74,9 +110,5 @@ private Thread mSplashThread;
     public void onAnimationRepeat(Animation animation) {
 
     }
-
-
-
-
 
 }
