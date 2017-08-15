269597891068caf90d8eaa1a03747860abf90ca5
HudaTabasi <htabasi95@gmail.com>
2017-08-15 11:30:50 +0300

    accountKit Part

--- app/src/main/java/com/shoppingapp/Activity/SplashActivtiy.java
+++ app/src/main/java/com/shoppingapp/Activity/SplashActivtiy.java
@@ -14,19 +14,9 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import com.shoppingapp.R;
 
-import com.facebook.accountkit.AccountKit;
-import com.facebook.accountkit.AccessToken;
-import com.facebook.accountkit.Account;
-import com.facebook.accountkit.AccountKitCallback;
-import com.facebook.accountkit.AccountKitError;
-import com.facebook.accountkit.AccountKitLoginResult;
-import com.facebook.accountkit.ui.AccountKitActivity;
-import com.facebook.accountkit.ui.AccountKitConfiguration;
-import com.facebook.accountkit.ui.LoginType;
+
 
 public class SplashActivtiy extends AppCompatActivity implements Animation.AnimationListener{
-    private static final long SPLASH_TIME_OUT = 5000;
-    private static final int APP_REQUEST_CODE = 99 ;
     // Animation
         Animation anima;
         Context mContext;
@@ -46,30 +36,6 @@ private Thread mSplashThread;
         // start the animation
         logo.startAnimation(anima);
 
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                if (AccountKit.getCurrentAccessToken() != null) {
-                    AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
-                @Override
-                public void onSuccess(final Account account) {
-                    Log.e("Error", "User Info Successfully");
-                    Intent i = new Intent(SplashActivtiy.this,MainActivity.class);
-                    startActivity(i);
-
-                }
-
-                @Override
-                public void onError(final AccountKitError error) {
-                    Log.e("Error", error.getUserFacingMessage());
-                    Toast.makeText(getApplicationContext(), error.getUserFacingMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
-
-                } else {
-                    phoneLogin();
-                }
-            }
-        }, SPLASH_TIME_OUT);
 
 
 //    mSplashThread =  new Thread(){
@@ -110,55 +76,7 @@ private Thread mSplashThread;
     }
 
 
-    public void phoneLogin() {
-        final Intent intent = new Intent(this, AccountKitActivity.class);
-        final AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder
-                = new AccountKitConfiguration.AccountKitConfigurationBuilder(
-                LoginType.PHONE,
-                AccountKitActivity.ResponseType.CODE);
-        final AccountKitConfiguration configuration = configurationBuilder.build();
-        intent.putExtra(
-                AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
-                configuration);
 
-        startActivityForResult(intent, APP_REQUEST_CODE);
-    }
-
-
-    @Override
-    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode != APP_REQUEST_CODE) {
-            return;
-        }
-
-        String toastMessage;
-        AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
-        if (loginResult == null || loginResult.wasCancelled()) {
-            toastMessage = "Login Cancelled";
-        } else if (loginResult.getError() != null) {
-            toastMessage = loginResult.getError().getErrorType().getMessage();
-
-        } else {
-            final AccessToken accessToken = loginResult.getAccessToken();
-            if (accessToken != null) {
-                toastMessage = "Success:" + accessToken.getAccountId();
-                Intent i = new Intent(SplashActivtiy.this,MainActivity.class);
-                startActivity(i);
-
-            } else {
-                toastMessage = "Unknown response type";
-            }
-
-
-            // Surface the result to your user in an appropriate way.
-            Toast.makeText(
-                    this,
-                    toastMessage,
-                    Toast.LENGTH_LONG)
-                    .show();
-        }
-    }
 
 
 }
