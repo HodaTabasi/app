bfd7f28cec1ec7fe1c86ffd15adbbebf014a250e
HudaTabasi <htabasi95@gmail.com>
2017-09-12 11:11:33 +0300

    ContextCompat.getDrawable(getContext(),R.drawable.***) instead of getContext().getResourcse().getDrawable(****)

--- app/src/main/java/com/shoppingapp/Fragment/ShopeFragment.java
+++ app/src/main/java/com/shoppingapp/Fragment/ShopeFragment.java
@@ -38,32 +38,32 @@ public class ShopeFragment extends Fragment {
         shopViewPager.setAdapter(adapter);
         shopTabLayout.setupWithViewPager(shopViewPager);
 
-        Drawable myDrawable = getContext().getResources().getDrawable(R.drawable.choth);
+        Drawable myDrawable = ContextCompat.getDrawable(getContext(), R.drawable.choth);
         myDrawable.setBounds(0,0,60,60);
         myDrawable.setTint(ContextCompat.getColor(getContext(),R.color.white));
         shopTabLayout.getTabAt(0).setIcon(myDrawable);
 
-        Drawable myDrawable1 = getContext().getResources().getDrawable(R.drawable.jeans);
+        Drawable myDrawable1 = ContextCompat.getDrawable(getContext(),R.drawable.jeans);
         myDrawable1.setBounds(0,0,60,60);
         myDrawable1.setTint(ContextCompat.getColor(getContext(),R.color.lighter_white));
         shopTabLayout.getTabAt(1).setIcon(myDrawable1);
 
-        Drawable myDrawable2 = getContext().getResources().getDrawable(R.drawable.shoes);
+        Drawable myDrawable2 = ContextCompat.getDrawable(getContext(),R.drawable.shoes);
         myDrawable2.setBounds(0,0,60,60);
         myDrawable2.setTint(ContextCompat.getColor(getContext(),R.color.lighter_white));
         shopTabLayout.getTabAt(2).setIcon(myDrawable2);
 
-        Drawable myDrawable3 = getContext().getResources().getDrawable(R.drawable.watches);
+        Drawable myDrawable3 = ContextCompat.getDrawable(getContext(),R.drawable.watches);
         myDrawable3.setBounds(0,0,60,60);
         myDrawable3.setTint(ContextCompat.getColor(getContext(),R.color.lighter_white));
         shopTabLayout.getTabAt(3).setIcon(myDrawable3);
 
-        Drawable myDrawable4 = getContext().getResources().getDrawable(R.drawable.bag);
+        Drawable myDrawable4 = ContextCompat.getDrawable(getContext(),R.drawable.bag);
         myDrawable4.setBounds(0,0,60,60);
         myDrawable4.setTint(ContextCompat.getColor(getContext(),R.color.lighter_white));
         shopTabLayout.getTabAt(4).setIcon(myDrawable4);
 
-        Drawable myDrawable5 = getContext().getResources().getDrawable(R.drawable.accessories);
+        Drawable myDrawable5 = ContextCompat.getDrawable(getContext(),R.drawable.accessories);
         myDrawable5.setBounds(0,0,60,60);
         myDrawable5.setTint(ContextCompat.getColor(getContext(),R.color.lighter_white));
         shopTabLayout.getTabAt(5).setIcon(myDrawable5);
